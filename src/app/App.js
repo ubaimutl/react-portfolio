4600132c34f3002243c9f1a2650320c04b61578d
ubai97 <ob.mtl97@gmail.coom>
2021-02-12 01:34:36 +0100

    simple page transtion added

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
   useLocation,
   withRouter,
 } from "react-router-dom";
+import { AnimatedSwitch } from "react-router-transition";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "../pages/home";
 import About from "../pages/about";
@@ -14,6 +14,8 @@ import ContactUs from "../pages/contact";
 import Headermain from "../header";
 import AnimatedCursor from "react-animated-cursor";
 import Socialicons from "../components/socialicons";
+import "./App.css";
+
 
 function _ScrollToTop(props) {
   const { pathname } = useLocation();
@@ -22,9 +24,7 @@ function _ScrollToTop(props) {
   }, [pathname]);
   return props.children;
 }
-
 const ScrollToTop = withRouter(_ScrollToTop);
-
 
 export default function App() {
   return (
@@ -42,13 +42,23 @@ export default function App() {
       <Router>
         <ScrollToTop>
           <Headermain />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/portfolio" component={Portfolio} />
-            <Route path="/contact" component={ContactUs} />
-            <Route path="*" component={Home} />
-          </Switch>
+          <Route
+            render={(i) => {
+              return (
+                <AnimatedSwitch
+                  atEnter={{ opacity: 0 }}
+                  atLeave={{ opacity: 0 }}
+                  atActive={{ opacity: 1}}
+                >
+                  <Route exact path="/" component={Home} />
+                  <Route path="/about" component={About} />
+                  <Route path="/portfolio" component={Portfolio} />
+                  <Route path="/contact" component={ContactUs} />
+                  <Route path="*" component={Home} />
+                </AnimatedSwitch>
+              );
+            }}
+          />
           <Socialicons />
         </ScrollToTop>
       </Router>
