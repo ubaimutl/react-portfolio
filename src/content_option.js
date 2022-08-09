1af81152ed737ed3256284635d54a9dff710a59b
ubaimutl <36690226+ubaimutl@users.noreply.github.com>
2022-08-09 13:13:53 +0200

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -139,6 +139,8 @@ const contactConfig = {
     YOUR_EMAIL: "name@domain.com",
     YOUR_FONE: "(555)123-4567",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
     YOUR_TEMPLATE_ID: "template_id",
     YOUR_USER_ID: "user_id",
