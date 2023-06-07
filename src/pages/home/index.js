e8e01604f81f40fa830248b69870055d724f6703
Mohammed Ali Abed <mohammedabedphs@gmail.com>
2023-06-06 17:22:41 -0700

    new

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -39,7 +39,7 @@ export const Home = () => {
                 </h1>
                 <p className="mb-1x">{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
-                  <Link to="/portfolio" className="text_2">
+                  <Link to="/photos" >
                     <div id="button_p" className="ac_btn btn ">
                       Our Podcast & Photos
                       <div className="ring one"></div>
