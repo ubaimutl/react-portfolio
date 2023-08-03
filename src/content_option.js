18b09aa337bec844058b92f0fccc1250c10429e0
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-08-02 20:40:19 -0400

    Edit front-page and about

--- src/content_option.js
+++ src/content_option.js
@@ -1,166 +1,288 @@
-const logotext = "JOHN";
+const logotext = "JoN";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+  title: "Jonathan Angeles",
+  description:
+    "I’m Jonathan Angeles, an entry level software developer with a passion for learning and creating. I am a recent graduate of the University of Texas at El Paso with a Bachelors in Computer Science. I am currently pursuing my master's degree at the prestigious Georgia Institute of Technology while looking for a full time position as a software developer.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
-    },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Jonathan Angeles",
+  animated: {
+    first: "I enjoy working on backend applications",
+    second: "I love problem solving",
+    third: "I develop amazing products",
+    fourth: "I love cars",
+  },
+  description:
+    "I'm a developer based in North Carolina, who loves to create and explore new technologies. I am currently pursuing my master's degree at the prestigious Georgia Institute of Technology while looking for a full time position as a software developer.",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+  title: "Learn about me",
+  aboutme:
+    "As a recent graduate with a passion for software engineering, I specialize in backend technologies while also dabbling in frontend development to craft innovative solutions for modern challenges. My expertise spans across both relational and nonrelational databases, seamlessly visualizing and interpreting data to deliver outstanding results. Determination fuels my pursuit of objectives, and my eagerness to learn enables me to adapt swiftly in the fast-paced tech landscape.\n\nCommunication is one of my greatest strengths, fearlessly engaging with others to foster seamless collaboration and effortlessly assume leadership roles. Working with diverse teams excites me, and I thrive in guiding and empowering others to excel. I yearn for opportunities that push me to grow and become an invaluable asset to any forward-thinking company, eager to bridge the gap between theoretical knowledge and real-world application.\n\nBeyond the realm of programming, I find joy in staying active, often exploring the breathtaking mountains of North Carolina through hiking and invigorating workouts. My passion for cars is evident in my role as a devoted car enthusiast, cherishing the exhilaration of driving my cherished Challenger. Beyond this, I take immense pleasure in sharing my programming knowledge with others, finding fulfillment in helping them succeed and witnessing their progress.\n\nEvery day, I strive to improve myself by at least 5%, maintaining a growth mindset and cherishing the value of wisdom. My entrepreneurial spirit shines through as I manage two thriving businesses alongside my father—a successful car dealership and a thriving shoe store.\n\nAs both a problem solver and a visionary, I'm dedicated to finding the most optimal solutions for programming challenges and business endeavors alike.\n\nI am not just a software engineer; I am a motivated, compassionate, and visionary individual who embraces challenges with an unyielding determination to inspire progress in both myself and those around me. I am ready to embark on a journey of growth and impact, bringing my unique blend of skills, passion, and entrepreneurial spirit to contribute meaningfully to your esteemed company.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+
+const worktimeline = [
+  {
+    jobtitle: "Data Structures Tutor",
+    where: "Mars Hill University",
+    date: "2022-2023",
+  },
+  {
+    jobtitle: "Inaugural STEM Program Mentor ",
+    where: "Mars Hill University",
+    date: "2022-2023",
+  },
+  {
+    jobtitle: "Information Technology Intern",
+    where: "Mars Hill University",
+    date: "Dec 2021 - Dec 2022",
+  },
 ];
 
-const skills = [{
-        name: "Python",
-        value: 90,
-    },
-    {
-        name: "Djano",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "Jquery",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "AWS S3",
+    value: 75,
+  },
+  {
+    name: "AWS",
+    value: 85,
+  },
+  {
+    name: "MongoDB",
+    value: 90,
+  },
+  {
+    name: "MySQL",
+    value: 90,
+  },
+  {
+    name: "PostgreSQL",
+    value: 85,
+  },
+  {
+    name: "MariaDB",
+    value: 95,
+  },
+  {
+    name: "C#",
+    value: 85,
+  },
+  {
+    name: "Java",
+    value: 90,
+  },
+  {
+    name: "JavaScript",
+    value: 90,
+  },
+  {
+    name: "Angular",
+    value: 80,
+  },
+  {
+    name: "ASP.NET",
+    value: 85,
+  },
+  {
+    name: "Spring Boot",
+    value: 85,
+  },
+  {
+    name: "React",
+    value: 95,
+  },
+  {
+    name: "Node.js",
+    value: 90,
+  },
+  {
+    name: "Express.js",
+    value: 90,
+  },
+  {
+    name: "Selenium",
+    value: 80,
+  },
+  {
+    name: "Docker",
+    value: 85,
+  },
+  {
+    name: "Jenkins",
+    value: 80,
+  },
+  {
+    name: "C/C++",
+    value: 75,
+  },
+  {
+    name: "PHP",
+    value: 80,
+  },
+  {
+    name: "Python",
+    value: 85,
+  },
+  {
+    name: "R",
+    value: 70,
+  },
+  {
+    name: "Material-UI",
+    value: 60,
+  },
+  {
+    name: "Bootstrap",
+    value: 70,
+  },
+  {
+    name: "TypeScript",
+    value: 80,
+  },
+  {
+    name: "Git",
+    value: 90,
+  },
+  {
+    name: "Linux",
+    value: 80,
+  },
+  {
+    name: "Postman",
+    value: 80,
+  },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
+  {
+    title: "Backend API Development",
+    description:
+      "With a strong foundation in backend development, I am well-versed in creating robust APIs that deliver outstanding results. I am proficient in a wide range of technologies, including Node.js, Express.js, Spring Boot, and ASP.NET.",
+  },
+  {
+    title: "Data Storage and Management",
+    description:
+      "I am well-versed in a wide range of database technologies, including MySQL, PostgreSQL, MariaDB, and MongoDB. I am also familiar with AWS S3 and AWS RDS.",
+  },
+  {
+    title: "Computation and Analysis",
+    description:
+      "I am proficient in a wide range of programming languages, including Java, C#, JavaScript, Python, and R. I am also familiar with C/C++ and PHP. I can leverage these languages to create powerful applications that deliver outstanding results. Easily adapatble, and able to analyze and interpret data to deliver meaningful insights.",
+  },
+  {
+    title: "Full Stack Development",
+    description:
+      "I am well-versed in both frontend and backend development, with a strong foundation in React, Angular, and ASP.NET. I am also familiar with Material-UI and Bootstrap and other frontend frameworks.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
 
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "name@domain.com",
+  YOUR_FONE: "(555)123-4567",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -31,7 +31,57 @@ export const About = () => {
           </Col>
           <Col lg="7" className="d-flex align-items-center">
             <div>
-              <p>{dataabout.aboutme}</p>
+              <p>
+                As a recent graduate with a passion for software engineering, I
+                specialize in backend technologies while also dabbling in
+                frontend development to craft innovative solutions for modern
+                challenges. My expertise spans across both relational and
+                nonrelational databases, seamlessly visualizing and interpreting
+                data to deliver outstanding results. Determination fuels my
+                pursuit of objectives, and my eagerness to learn enables me to
+                adapt swiftly in the fast-paced tech landscape.
+              </p>
+              <p>
+                Communication is one of my greatest strengths, fearlessly
+                engaging with others to foster seamless collaboration and
+                effortlessly assume leadership roles. Working with diverse teams
+                excites me, and I thrive in guiding and empowering others to
+                excel. I yearn for opportunities that push me to grow and become
+                an invaluable asset to any forward-thinking company, eager to
+                bridge the gap between theoretical knowledge and real-world
+                application.
+              </p>
+              <p>
+                Beyond the realm of programming, I find joy in staying active,
+                often exploring the breathtaking mountains of North Carolina
+                through hiking. I have a passion for cars and love the
+                exhilaration of driving my cherished Challenger. Beyond this, I
+                take immense pleasur in teaching and sharing my knowledge with
+                others, finding fulfillment in helping them succeed and
+                witnessing their progress.
+              </p>
+              <p>
+                Every day, I strive to improve myself by at least 5%,
+                maintaining a growth mindset and cherishing the value of wisdom.
+                I am an incredibly fast learner and can adapt to any situation.
+                My entrepreneurial spirit shines through as I manage two
+                thriving businesses alongside my father—a successful car
+                dealership and a thriving shoe store.
+              </p>
+              <p>
+                As both a problem solver and a visionary, I'm dedicated to
+                finding the most optimal solutions for programming challenges
+                and business endeavors alike.
+              </p>
+              <p>
+                I am not just a software engineer; I am a motivated,
+                compassionate, and thoughtful individual who embraces challenges
+                with an unyielding determination to inspire progress in both
+                myself and those around me. I am ready to embark on a journey of
+                growth and impact, bringing my unique blend of skills, passion,
+                and entrepreneurial spirit to contribute meaningfully to your
+                esteemed company.
+              </p>
             </div>
           </Col>
         </Row>
@@ -81,7 +131,7 @@ export const About = () => {
         </Row>
         <Row className="sec_sp">
           <Col lang="5">
-            <h3 className="color_sec py-4">services</h3>
+            <h3 className="color_sec py-4">Services</h3>
           </Col>
           <Col lg="7">
             {services.map((data, i) => {
