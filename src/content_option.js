2602752606da5a7d72862a70146650c78d105401
jaskirat singh <sjaskirat247@gmail.com>
2023-11-01 13:38:57 -0600

    .

--- src/content_option.js
+++ src/content_option.js
@@ -1,19 +1,22 @@
-const logotext = "JOHN";
+import jas_pic from "./assets/images/jas_pic.jpg"
+
+const logotext = "Jaskirat Singh";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Jaskirat Singh",
+    description: "I’m Jaskirat Singh Web Designer and Developer _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Jaskirat Singh",
 
-ted: {
+animated: {
         first: "I love coding",
-   second: "I code cool websites",
+        second: "I code cool websites",
         third: "I develop mobile apps",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    // your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    your_img_url: jas_pic,
 };
 
 const dataabout = {
