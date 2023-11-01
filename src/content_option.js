46279bc32e1af69786c540a4452ac6279a27be56
jaskirat singh <sjaskirat247@gmail.com>
2023-11-01 13:23:11 -0600

    main image changed

--- src/content_option.js
+++ src/content_option.js
@@ -1,18 +1,19 @@
-const logotext = '<img src="./img/jas_portfolio.jpg" alt="Logo Image">';
+const logotext = "JOHN";
 const meta = {
-    title: "Jaskirat Singh",
-    description: "I’m Jaskirat Singh data scientist _ Full stack devloper,currently working in Berlin",
+    title: "John Doe",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m Jaskirat Singh",
-    animated: {
+    title: "I’m John Doe",
+
+ted: {
         first: "I love coding",
-        second: "I code cool websites",
+   second: "I code cool websites",
         third: "I develop mobile apps",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: '<img src="img/jas_portfolio.jpg" alt="Logo Image">',
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
